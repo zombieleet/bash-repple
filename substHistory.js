6da78b7fcf90b4797e022051f9522cbbc7c04575
Victory Osikwemhe <zombieleetnca@gmail.com>
2016-12-17 12:40:32 +0100

    Remove Trailing white space

--- substHistory.js
+++ substHistory.js
@@ -1,5 +1,33 @@
-function substHistory(cmd) {
+let histArray = require('./history').histArray
+function substHistory(cmd,callback) {
+    let res;
 
-    
-    
+    //console.log(/^\$(\^.*)(\^.*)/.test(cmd))
+
+    if ( /^\$(\^.*)(\^.*)/.test(cmd) ) {
+
+	//console.log('hi')
+	let subst = cmd.match(/^\$(\^.*)(\^.*)/)
+
+	let match = new RegExp(subst[1].replace('^',''), 'g');
+
+	let matchAgainst = subst[2].replace('^','');
+
+	res = (histArray[histArray.length - 1]).replace(match,matchAgainst);
+
+	histArray.push(res)
+
+	console.log(histArray);
+
+	callback(null, res);
+
+	return true;
+
+    }
+
 };
+
+
+module.exports = {
+    substHistory
+}
